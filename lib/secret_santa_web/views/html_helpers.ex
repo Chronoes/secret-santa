c9894d6562e70480b65b2758c36a5d518a5cb0d0
Chronoes <marten@tarkin.ee>
2020-11-12 22:59:10 +0200

    Changed link colors for better readability Made wish box more clear for gifter Version 0.5.1

--- lib/secret_santa_web/views/html_helpers.ex
+++ lib/secret_santa_web/views/html_helpers.ex
@@ -22,12 +22,4 @@ defmodule SecretSantaWeb.HTMLHelpers do
   defp link_element(url, opts) do
     link(url, Keyword.merge(opts, to: url, target: "_blank", rel: "noreferrer noopener"))
   end
-
-  @spec unsafe_to_safe(list :: [Phoenix.HTML.unsafe() | Phoenix.HTML.safe()], opts :: keyword()) :: [
-          Phoenix.HTML.safe()
-        ]
-  def unsafe_to_safe(list, opts \\ [])
-  def unsafe_to_safe([], _opts), do: []
-  def unsafe_to_safe([{:safe, a} | rest], opts), do: [{:safe, a} | unsafe_to_safe(rest, opts)]
-  def unsafe_to_safe([unsafe | rest], opts), do: [text_to_html(unsafe, opts) | unsafe_to_safe(rest, opts)]
 end
